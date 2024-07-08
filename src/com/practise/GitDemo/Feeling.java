c61b9314f8969ff55b401f7e2a83f7145e1a9549
laksh <laksh@Hanvika>
2024-07-08 15:01:58 +0530

    First Commit

--- src/com/practise/GitDemo/Feeling.java
+++ src/com/practise/GitDemo/Feeling.java
@@ -5,7 +5,7 @@ public class Feeling {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String hungry="yes";
+		String hungry="NO";
 		if(hungry=="yes")
 			System.out.println("Eat Something Dear");
 		else
